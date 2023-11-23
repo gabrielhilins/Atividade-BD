1e0d48b945fa2d5159ac89095ca44c2c7582fa3b
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2023-11-23 10:59:04 -0300

    Update selects grupo 2.sql

--- Banco de dados/Arquivos SQL/selects grupo 2.sql
+++ Banco de dados/Arquivos SQL/selects grupo 2.sql
@@ -1,1 +1,22 @@
 -- Alunos: Luiz Henrique e Yuri Catunda
+
+-- busque o nome e idade de todos os alunos com idade maior que 10
+select a.nome_aluno, a.idade
+from Aluno a
+where idade > 10;
+
+-- calcule a media de notas por série e exiba a serie
+-- e o resultado em ordem crescente
+select serie, avg(nota)
+from resultados r
+group by r.serie
+order by avg(nota);
+
+-- Liste em ordem alfabetica o nome dos professores que
+-- trabalham em escolas no recife e suas disciplinas
+select p.nome_prof, d.nome_disciplina
+from professores p, Disciplinas d
+where exists(select nome_cidade
+            from cidade
+            where nome_cidade = 'Recife')
+order by nome_prof;
