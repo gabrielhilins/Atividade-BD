4a52e9149c65e51c655bb23ebd4f7d861f5145db
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2023-11-24 08:12:14 -0300

    selects grupo 2.sql

--- Banco de dados/Arquivos SQL/selects grupo 2.sql
+++ Banco de dados/Arquivos SQL/selects grupo 2.sql
@@ -1,18 +1,18 @@
 -- Alunos: Luiz Henrique e Yuri Catunda
-
--- busque o nome e idade de todos os alunos com idade maior que 10
+            
+ --1. (facil) busque o nome e idade de todos os alunos com idade maior que 10
 select a.nome_aluno, a.idade
 from Aluno a
 where idade > 10;
 
--- calcule a media de notas por série e exiba a serie
+--(media) calcule a media de notas por série e exiba a serie
 -- e o resultado, sendo o resultado em ordem decrescente
 select serie, avg(nota)
 from resultados r
 group by r.serie
 order by avg(nota) desc;
 
--- Liste o nome dos professores, em ordem alfabetica, e suas disciplinas que
+-- (dificil) Liste o nome dos professores, em ordem alfabetica, e suas disciplinas que
 -- trabalham em escolas no recife
 select p.nome_prof, d.nome_disciplina
 from professores p
@@ -23,13 +23,13 @@ where exists(select nome_cidade
             where nome_cidade = 'Recife')
 order by nome_prof;
 
---busque o id e as notas dos alunos que as notas são maiores que 8
+--(menos facil) busque o id e as notas dos alunos que as notas são maiores que 8
 select r.id_aluno, r.nota
 from resultados r
 left join matricula m on r.id_aluno = m.id_aluno
 where r.nota > 8;
 
---busque o nome, o id e o codigo da disciplina de todos os professores
+--(menos facil) busque o nome, o id e o codigo da disciplina de todos os professores
 -- que dão aula no ensino medio
 select p.nome_prof, p.id_func, d.cod_disciplina
     from professores p
