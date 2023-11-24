46cde87e17ee88b5257ef5cd61327edcd483ab84
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2023-11-24 09:19:36 -0300

    Update selects grupo 1.sql

--- Banco de dados/Arquivos SQL/selects grupo 1.sql
+++ Banco de dados/Arquivos SQL/selects grupo 1.sql
@@ -37,7 +37,7 @@ LEFT JOIN Administradores A ON F.id_func = A.id_func AND F.cod_escola = A.cod_es
 LEFT JOIN Saude S ON F.id_func = S.id_func AND F.cod_escola = S.cod_escola
 LEFT JOIN Seguranca SG ON F.id_func = SG.id_func AND F.cod_escola = SG.cod_escola;
 
--- Selecione os nomes e id dos alunos e profressores que estão em salas de Recife
+-- Selecione os nomes e id dos alunos e profressores que estão em salas de São Paulo
 SELECT A.id_aluno, A.nome_aluno, P.nome_prof AS nome_professor, P.id_func AS id_prof
 FROM Aluno A
 JOIN Matricula M ON A.id_aluno = M.id_aluno
@@ -45,7 +45,7 @@ JOIN Sala S ON M.cod_escola = S.cod_escola AND M.num_sala = S.num_sala AND M.ser
 JOIN Professores P ON S.cod_escola = P.cod_escola AND S.num_sala = P.nu_sala AND S.serie = P.serie
 JOIN Escola E ON S.cod_escola = E.cod_escola
 JOIN Cidade C ON E.cod_cidade = C.cod_cidade
-WHERE C.nome_cidade = 'Recife';
+WHERE C.nome_cidade = 'São Paulo';
 
 -- SELECT ANINHADO
 -- Busque o id do aluno(a) que estuda numa serie '3C' e o nome dele(a)(s) (ordene por nome)
